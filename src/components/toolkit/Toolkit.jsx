3f0b443e5a97fb9206a6aae217884edf15dc21d6
misterraina <ayushraina617@gmail.com>
2024-06-29 23:19:03 +0530

    toolkit responsiveness correct

--- src/components/toolkit/Toolkit.jsx
+++ src/components/toolkit/Toolkit.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 export default function Toolkit() {
-    return (
-      <div className='flex pb-6 items-center justify-center '>
-       
-
-<span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">#HTML</span>
-<span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">#Css</span>
-<span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">#JavaScript</span>
-<span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">#React</span>
-<span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">#MongoDb</span>
-<span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">#NodeJs</span>
-<span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">#Express</span>
-<span className="bg-gray-100 text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300">#Postgresql</span>
-
-
-      </div>
-    )
-  }
-  
+  return (
+    <div className="flex flex-wrap pb-6 items-center justify-center space-x-2 space-y-2">
+      {['HTML', 'CSS', 'JavaScript', 'React', 'MongoDb', 'NodeJs', 'Express', 'Postgresql'].map((item) => (
+        <span
+          key={item}
+          className="bg-gray-100 text-gray-800 text-sm font-medium px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300"
+        >
+          #{item}
+        </span>
+      ))}
+    </div>
+  );
+}
