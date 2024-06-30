0edd0efac19ded2b09cfaeaf688981146bdae325
misterraina <ayushraina617@gmail.com>
2024-06-30 15:15:49 +0530

    cli add

--- src/components/cliWindowIframe/CommandPaletteIframe.jsx
+++ src/components/cliWindowIframe/CommandPaletteIframe.jsx
@@ -4,7 +4,8 @@ const CommandPaletteIframe = () => {
   return (
     <div id='sectionCli' className="command-palette-iframe">
       <iframe
-        src="http://localhost:3000/"
+        // src="http://localhost:3000/"
+        src="https://terminal-cli.vercel.app/"
         title="Command Palette"
         width="100%"
         height="600px"
